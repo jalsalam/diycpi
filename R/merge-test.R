12d8560618e04d3147c9f300bb0eeaa37618cb5f
Nabeel Alsalam <nabeel.alsalam@gmail.com>
2017-03-08 10:15:43 -0500

    An experiment by Nabeel

--- R/merge-test.R
+++ R/merge-test.R
@@ -19,5 +19,5 @@
 #This is Jameel's next change.
 
 #5 - this one is different. Now we will have different branches. Jameel will change/commit/push. Nabeel will branch/change/commit/push.
-#[change thiS LINE]
+# This is an experiment by Nabeel to do somehting completely different.
 #this won't conflict.
