def5793a5864b2331c61a43d97f5c82924a360bd
Jameel Alsalam <jalsalam@gmail.com>
2017-03-08 09:19:20 -0500

    setup merge-test

---
+++ R/merge-test.R
@@ -0,0 +1,11 @@
+#merge-test.R
+
+#This is a test file intended for testing out GitKraken steps with slight merge conflicts
+
+#First here, Jameel will change/commit/push. Then Nabeel will change/(not commit)/pull FF if possible:
+#[Change this line #1]
+#I think this will result in a stash
+
+#Then here, Jameel will change/commit/push. Then Nabeel will change/commit/push. This will give error. So you will pull and it will report merge conflicts. You will resolve the merge conflicts, commit/push.
+#[Change this line #2]
+#This will result in an extra "merge commit"
